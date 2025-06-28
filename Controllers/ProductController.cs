3cd6ed9a1ad0c2e70d132b67fa1b90cfff9a448b
Daniel Nice <Danielpogi90@gmail.com>
2025-06-29 03:15:35 +0800

    Improve form styling for readability in light and dark mode

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Threading.Tasks;
 
-[AllowAnonymous] 
+[Authorize]
 public class ProductController : Controller
 {
     private readonly ApplicationDbContext _context;
